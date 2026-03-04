f83533756c74a9e9557669e376431198cae10bf6
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-03-04 10:57:00 +0530

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,19 @@
+body{
+font-family: Arial;
+padding:40px;
+}
+
+button{
+padding:10px 20px;
+background:#25D366;
+border:none;
+color:white;
+cursor:pointer;
+margin-top:10px;
+}
+
+a{
+display:block;
+margin-top:10px;
+font-size:16px;
+}
